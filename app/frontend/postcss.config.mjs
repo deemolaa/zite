09723217296c5c6cf4c61c1d117409d52986e91f
deemolaa <viralblaq@gmail.com>
2025-11-01 07:46:04 +0100

    production: ready.

--- app/frontend/postcss.config.mjs
+++ app/frontend/postcss.config.mjs
@@ -1,9 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
-    autoprefixer: {}
+    autoprefixer: {},
   },
 };
-
-export default config;
